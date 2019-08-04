cce904e3018fd273ab3bb6c6997badf8458486d3
Chris <cthuku@fintech-group.com>
2019-08-04 22:27:12 +0300

    Money Extensions

--- Samurai.Domain/ValueObjects/Money.cs
+++ Samurai.Domain/ValueObjects/Money.cs
@@ -6,9 +6,10 @@ namespace Samurai.Domain.ValueObjects
 	public class Money
 	{
 		//This is a full Value object now
-		public static Money Create(string currency, decimal amount, DateTime time)
+		public static Money Create(string currency, decimal amount, DateTime? time)
 		{
-			return new Money(currency, amount, time);
+			DateTime t = time != null ? time.Value : DateTimeRangeExtensions.GetDate();
+			return new Money(currency, amount, t);
 		}
 		private Money() { }
 		//Enum Currencies ????????????????????????????????????????????????????????????????
@@ -19,7 +20,6 @@ namespace Samurai.Domain.ValueObjects
 		public string Currency { get; private set; } // Private set or just get operator makes it Immutable ==> Private set Coz of EF
 		public decimal Amount { get; private set; } // Make's sure values cannot be changes individually 
 		public DateTime Time { get; private set; }
-		public string GetSalary => $"{Currency} {Amount.ToString("N2")}";
 		public override bool Equals(object obj)
 		{
 			return obj is Money money &&
@@ -44,6 +44,19 @@ namespace Samurai.Domain.ValueObjects
 		public static bool operator !=(Money left, Money right)
 		{
 			return !(left == right);
+		}
+	}
+	public static class MoneyExtensions
+	{
+		public static string ToMoneyString(this Money money, bool reverse = false)
+		{
+			if(reverse) return $"{money.Amount.ToString("N2")} {money.Currency}";
+			return $"{money.Currency} {money.Amount.ToString("N2")}";
+		}
+		public static Money GetDifference(this Money money, Money _money)
+		{
+			if (money.Currency != _money.Currency) throw new Exception("Different currencies");
+			return Money.Create(money.Currency, Math.Abs(money.Amount - _money.Amount), DateTimeRangeExtensions.GetDate());
 		}
 	}
 }
